c65aa3948fa42541d61939a1619b7b99a847296f
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-28 19:15:15 -0700

    fixed paths.. tests run but dont return anything, paths generated properly.

--- chronAmericaApi.php
+++ chronAmericaApi.php
@@ -17,20 +17,21 @@ class chronAmericaApi extends APIBaseClass{
 		return $data;	
 	}
 	
-	public function title_search($terms,$page=NULL,$format='json'){
+	public function title_search($terms,$format='json',$page=NULL){
 	// also supports json and atom defaults to HTML
 		// can't use array_combine because if arrays are inequal it returns false
-		$data = self::filter_params(array('terms','page','format'),func_get_args() );
-		return self::_request(self::$api_url."/search/titles/results", 'get', array_filter($data));
+		$data = self::filter_params(array('terms','format','page'),func_get_args() );
+
+		return self::_request("/search/titles/results", 'GET', array_filter($data));
 	}
 	
-	public function page_search($andtext,$page=NULL,$format='json'){
+	public function page_search($andtext,$format='json',$page=NULL){
 		$data = self::filter_params(array('terms','page','format'),func_get_args() );
-		return self::_request(self::$api_url."/search/titles/results", 'get', array_filter($data));
+		return self::_request("/search/titles/results", 'get', array_filter($data));
 	}
 	
 	public function suggest_titles($q){
 	// experimental should auto return JSON
-		return self::_request(self::$api_url."/suggest/titles/", 'get', array('q'=>$q));
+		return self::_request("/suggest/titles/", 'get', array('q'=>$q));
 	}
 }
--- example_use.php
+++ example_use.php
@@ -6,5 +6,9 @@ require 'chronAmericaApi.php';
 
 $new = new chronAmericaApi();
 
+echo $new->title_search('michigan');
+echo $new->title_search('michigan','atom');
+echo $new->title_search('michigan','json');
+
 // Debug information
 die(print_r($new).print_r(get_object_vars($new)).print_r(get_class_methods(get_class($new))));
